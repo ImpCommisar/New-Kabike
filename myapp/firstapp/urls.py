b9eaa7cbf11860e3c00e9ee501eb66b1ed73a709
ImpCommisar <javierbolong@gmail.com>
2024-01-14 19:50:20 +0800

    completed the youtube video, going through lectures to clarify

--- myapp/firstapp/urls.py
+++ myapp/firstapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name= 'home'),
+    path("todos/", views.todos, name = "Todos")
 ]
 # This might be needed, depending on your Django version
 app_name = "firstapp"
