5fb30bc750b6896fc4a26f50993d48648392b3ca
ImpCommisar <javierbolong@gmail.com>
2024-01-18 11:50:50 +0800

    Finished through admin panel in lecture 10

--- myapp/firstapp/admin.py
+++ myapp/firstapp/admin.py
@@ -1,5 +1,34 @@
 from django.contrib import admin
-from .models import TodoItem
+from .models import TodoItem, subject, IndexCard
+
+class IndexCardInline(admin.TabularInline):
+    model = IndexCard
+
+class subjectAdmin(admin.ModelAdmin):
+    model = subject
+
+    # Use the subject name and the teacher's name to search through all instances
+    search_fields = ('name', 'teacher_name',)
+    # Display just the teacher name and the subject name in the list
+    list_display = ('name', 'teacher_name',)
+    # Enable filtering via the teacher's name and the number of units
+    list_filter = ('teacher_name', 'units',)
+
+    inlines = [IndexCardInline,]
+
+    fieldsets = [
+        ('Class Data', {
+            'fields': [
+                ('name', 'units'), 'teacher_name'
+            ]
+        })
+    ]
+    
+class IndexCardAdmin(admin.ModelAdmin):
+    model = IndexCard
+
 
 # Register your models here.
+admin.site.register(subject, subjectAdmin)
+admin.site.register(IndexCard, IndexCardAdmin)
 admin.site.register(TodoItem)
--- myapp/firstapp/models.py
+++ myapp/firstapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
-""" class subject(models.Model):
+class subject(models.Model):
     name = models.CharField(max_length = 100)
     teacher_name = models.CharField(max_length = 50)
     units = models.IntegerField()
@@ -13,9 +13,9 @@ from django.urls import reverse
         return reverse('sibject_detail', args = [str(self.name)])
 
     def is_tutorial(self):
-        return self.units == 1 """
+        return self.units == 1
     
-""" class IndexCard(models.Model):
+class IndexCard(models.Model):
     name = models.CharField(max_length = 100)
     subject = models.ForeignKey(
         subject, 
@@ -23,7 +23,7 @@ from django.urls import reverse
         related_name = 'students'
         )
     section = models.CharField(max_length=5)
-    age = models.IntegerField()  """
+    age = models.IntegerField() 
 
 class TodoItem(models.Model):
     title = models.CharField(max_length = 200)
