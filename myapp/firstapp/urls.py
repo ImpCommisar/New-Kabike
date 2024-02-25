db75cbef7aed4481082d8402125b0bcc37241534
ImpCommisar <javierbolong@gmail.com>
2024-02-25 18:13:07 +0800

    almost done with video 2 of google maps, integrated googlemaps into code

--- myapp/firstapp/urls.py
+++ myapp/firstapp/urls.py
@@ -5,10 +5,9 @@ from .views import *
 urlpatterns = [
     #path('', views.home, name= 'home'),
     path('', GoogleView.as_view(), name = "Google_View"),
-    path('geocoding/<int:pk>', GeoCodeView.as_view(), name = 'Geo_View'),
+    path('geocoding/<int:pk>', GeoCodeView.as_view(), name = 'GeoView'),
     path("todos/", views.todos, name = "Todos"),
     path("api/", views.getData),
     path("add/", views.addItem),
 ]
 # This might be needed, depending on your Django version
-app_name = "firstapp"
--- myapp/firstapp/views.py
+++ myapp/firstapp/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView
 from django.views import View
 from .models import TodoItem
 from .models import *
+import googlemaps
+from django.conf import settings
 # Respnse takes any serialized or python data, and renders it as json data
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -44,8 +46,36 @@ class GeoCodeView(View):
     def get(self, request, pk):
         location = locations.objects.get(pk = pk)
 
+        if location.address and location.country and location.city != None:
+            address_string = str(location.address)+ "," + str(location.city) + ", " +str (location.country)
+            gmaps = googlemaps.Client(key = settings.GOOGLE_API_KEY)
+            result = gmaps.geocode(address_string)[0]
+
+            lat = result.get('geometry', {}).get('location', None).get('lat', None)
+            lng = result.get('geometry', {}).get('location', None).get('lng', None)
+            place_id = result.get('place_id', {})
+
+            location.lat = lat
+            location.lng = lng
+            location.place_id = place_id
+            location.save()
+
+            """ Old method to locate stuff (fill in context and html to re use this) """
+            """ geometry = result.get('geometry', {})
+            
+            location1 = geometry.get('location', {})
+
+            lat = location1.get('lat', None)
+            lng = location1.get('lng', None) """
+        else:
+            result = ""   
+
         context = {
-            'location' :location
+            'location' :location,
+            'result':result,
+            'lat': lat,
+            'lng' : lng,
+            'place_id' : place_id
         }
 
         return render(request, self.template_name, context)
